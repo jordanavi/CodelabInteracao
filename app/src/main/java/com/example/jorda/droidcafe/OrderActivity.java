e51b551657aec98222154739d10c80292ed8975c
jordanavi <jordanavilela01@gmail.com>
2018-11-28 20:18:38 -0200

    Usando Spinner

--- app/src/main/java/com/example/jorda/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/jorda/droidcafe/OrderActivity.java
@@ -2,12 +2,54 @@ package com.example.jorda.droidcafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()){
+           case R.id.sameday:
+                if(checked)
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if(checked)
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if(checked)
+                    displayToast(getString(R.string.pick_up));
+                break;
+           default:
+               break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
