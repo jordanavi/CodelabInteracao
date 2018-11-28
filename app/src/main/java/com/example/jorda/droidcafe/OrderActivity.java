c2004c41b0d7e267a99f6b0d1a8b1b59856d60f9
jordanavi <jordanavilela01@gmail.com>
2018-11-28 21:02:47 -0200

    Instanciacao do Spinner no metodo onCreate

--- app/src/main/java/com/example/jorda/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/jorda/droidcafe/OrderActivity.java
@@ -4,18 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
-
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+    }*/
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        // ... Rest of onCreate code ...
+        // Create the spinner.
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
 
-        
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+        // ... End of onCreate code ...
     }
 
     public void onRadioButtonClicked(View view) {
